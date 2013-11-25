54ea4c3bb9fb69effe580a1600c1498ccf957d4e
Massimo Ronca <massimo@mikamai.com>
2013-11-25 16:44:40 +0100

    added support for urls in the format (width)x(height) (eg: 400x300)

--- main.go
+++ main.go
@@ -11,7 +11,18 @@ func main() {
 
 	router.Get("/", RootHandler)
 
-	router.Get(`/:width/?(:height/?)?`, ImageHandler).
+	// match (width)x(height) format
+	// we cannot choose which character set the named routes can take
+	// they only stop matching on / # ? ( ) . \
+	// we should be able to do something like
+	// (:width=[\d+])(x(:height=[\d+]))?
+	// or something even simplier like
+	// (:width=:digits)(x(:height=:digits))(.(:format=json|xml|atom))
+	router.Get(`/(?P<width>\d+)x(?P<height>\d+)?/?`, ImageHandler).
+		AddBeforeFilter(RequireValidImageParameters).
+		AddBeforeFilter(GenerateImageCache)
+
+	router.Get(`/:width/?(:height)/?`, ImageHandler).
 		AddBeforeFilter(RequireValidImageParameters).
 		AddBeforeFilter(GenerateImageCache)
 
