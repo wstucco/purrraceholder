13e6486113c5028d877c00c65d006e51db4fadbf
Massimo Ronca <massimo@mikamai.com>
2013-11-28 18:37:11 +0100

    added notfound_handler test

---
+++ notfound_handler_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNotFoundHandler(t *testing.T) {
+	recorder := newTestRequest("GET", "/notfound")
+	expectedStatusCode := 404
+	if recorder.Code != expectedStatusCode {
+		t.Errorf("Expected response status code `%d`, got `%d`", expectedStatusCode, recorder.Code)
+	}
+}
