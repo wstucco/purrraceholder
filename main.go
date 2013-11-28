bb72dbcaae9a967b985d757a141856112ff24a50
Massimo Ronca <massimo@mikamai.com>
2013-11-28 18:46:19 +0100

    fixed regexp for <width>x<height>, height is optional even if x is present

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ func main() {
 	// (:width=[\d+])(x(:height=[\d+]))?
 	// or something even simplier like
 	// (:width=:digits)(x(:height=:digits))(.(:format=json|xml|atom))
-	router.Get(`/(?P<width>\d+)(x(?P<height>\d+))?/?`, ImageHandler).
+	router.Get(`/(?P<width>\d+)(x(?P<height>\d+)?)?/?`, ImageHandler).
 		AddBeforeFilter(RequireValidImageParameters).
 		AddBeforeFilter(GenerateImageCache)
 
-	router.Get(`/:width/?(:height)/?`, ImageHandler).
+	router.Get(`/:width/?(:height)?/?`, ImageHandler).
 		AddBeforeFilter(RequireValidImageParameters).
 		AddBeforeFilter(GenerateImageCache)
 
