0a26ff4f009d3f4fd2a7bef955bea63cc2bdaab2
Massimo Ronca <massimo@mikamai.com>
2013-12-03 11:16:45 +0100

    root_handler_test now checks for some known text

--- root_handler_test.go
+++ root_handler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -9,5 +10,9 @@ func TestRootHandler(t *testing.T) {
 	expectedStatusCode := 200
 	if recorder.Code != expectedStatusCode {
 		t.Errorf("Expected response status code `%d`, got `%d`", expectedStatusCode, recorder.Code)
+	}
+
+	if !strings.Contains(recorder.Body.String(), "Welcome to the Purrraceholder") {
+		t.Errorf("Expected welcome message in home was not found")
 	}
 }
